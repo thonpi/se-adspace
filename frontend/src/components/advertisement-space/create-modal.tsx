4a2537063f022236b5e1d0f0e1fe1c5d30581ec1
thonDev <thon-theng@mptc.gov.kh>
2025-06-04 18:05:59 +0700

    ref: update color

--- frontend/src/components/advertisement-space/create-modal.tsx
+++ frontend/src/components/advertisement-space/create-modal.tsx
@@ -129,7 +129,7 @@ export default function CreateAdvertisementModal({
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
                 value={formik.values[field] as string | number}
-                className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:ring focus:ring-indigo-200"
+                className="mt-1 block w-full border text-black border-gray-300 rounded-md px-3 py-2 shadow-sm focus:ring focus:ring-indigo-200"
               />
               {formik.touched[field] && formik.errors[field] && (
                 <p className="text-sm text-red-500 mt-1 text-right">
